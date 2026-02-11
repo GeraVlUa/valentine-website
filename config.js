c86dff808ccdaa7452a33b74d69b354857e979d2
Volodymyr Herasymchuk <vherasymchuk@varteq.com>
2026-02-11 18:25:16 +0000

    Updating texts

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Invite you to celebrate our love together at Shere Ln, Shere, today at 3pm. Can't wait to see you there! 💖💝💓",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
